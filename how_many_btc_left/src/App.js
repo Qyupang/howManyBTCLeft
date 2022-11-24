dc0f222cdf26fb907e283eb1ffebba8026d99810
Qyupang <hyunkyu0509@naver.com>
2022-11-24 14:24:18 +0900

    Feat: add timer

--- how_many_btc_left/src/App.js
+++ how_many_btc_left/src/App.js
@@ -11,6 +11,7 @@ const ChartContainer = styled.div`
   flex-direction: column;
   color: white;
   align-items: center;
+  font-weight: 600;
 
   h1 {
     font-size: 150px;
@@ -21,27 +22,43 @@ const ChartContainer = styled.div`
 function App() {
   const [coin, setCoin] = useState();
   const [loading, setLoading] = useState(true);
+  const [minutes, setMinutes] = useState(10);
+  const [seconds, setSeconds] = useState(0);
 
   useEffect(() => {
-    fetch('https://api.coinpaprika.com/v1/tickers/btc-bitcoin')
-      .then((response) => response.json())
-      .then((json) => {
-        setCoin(json);
-        setLoading(false);
-        return json;
-      });
-  }, []);
+    const countdown = setInterval(() => {
+      if (parseInt(seconds) > 0) {
+        setSeconds(parseInt(seconds) - 1);
+      }
+      if (parseInt(seconds) === 0) {
+        if (parseInt(minutes) === 0) {
+          // clearInterval(countdown);
+          setMinutes(10);
+          setSeconds(0);
+        } else {
+          setMinutes(parseInt(minutes) - 1);
+          setSeconds(59);
+        }
+      }
+    }, 1000);
+    return () => clearInterval(countdown);
+  }, [minutes, seconds]);
 
-  // setInterval(() => {
-  //   fetch('https://api.coinpaprika.com/v1/tickers/btc-bitcoin')
-  //     .then((response) => response.json())
-  //     .then((json) => {
-  //       setCoin(json);
-  //       setLoading(false);
-  //       return json;
-  //     })
-  //     .then((coin) => console.log(coin));
-  // }, 100000);
+  useEffect(() => {
+    const fetchData = () => {
+      fetch('https://api.coinpaprika.com/v1/tickers/btc-bitcoin')
+        .then((response) => response.json())
+        .then((json) => {
+          setCoin(json);
+          setLoading(false);
+          return json;
+        })
+        .then((coin) => console.log(coin));
+    };
+    fetchData();
+
+    setInterval(fetchData, 600000);
+  }, []);
 
   const data = {
     color: 'white',
@@ -74,7 +91,14 @@ function App() {
           <div style={{ marginBottom: '50px' }}>
             Left: {coin.max_supply - coin.total_supply}
           </div>
+          <h3>
+            {minutes}:{seconds < 10 ? `0${seconds}` : seconds}
+          </h3>
           <Pie data={data} />
+          <div>
+            {coin.total_supply} / {coin.max_supply}(
+            {Math.round(100 - (coin.total_supply / coin.max_supply) * 100)} %)
+          </div>
         </div>
       )}
     </ChartContainer>
