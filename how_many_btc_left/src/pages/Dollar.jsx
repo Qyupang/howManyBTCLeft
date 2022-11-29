3075752e5da76330f66fa4ccda1cbfe7afea16e6
Qyupang <hyunkyu0509@naver.com>
2022-11-30 01:09:39 +0900

    Feat: add dollar chart

--- how_many_btc_left/src/pages/Dollar.jsx
+++ how_many_btc_left/src/pages/Dollar.jsx
@@ -1,5 +1,27 @@
 import React, { useEffect, useState } from 'react';
-import './dollar.scss';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend
+);
 
 const Dollar = () => {
   const [dollar, setDollar] = useState();
@@ -7,18 +29,61 @@ const Dollar = () => {
 
   useEffect(() => {
     const fetchData = () => {
-      fetch(
-        'https://api.stlouisfed.org/fred/series?series_id=WM1NS&realtime_start=1975-01-06&realtime_end=2022-11-28&api_key=process.env.REACT_APP_FED_API_KEY&file_type=json'
-      ).then((response) => {
-        setDollar(response);
-        setLoading(false);
-      });
+      fetch('https://www.econdb.com/api/series/GDEBTUS/?format=json')
+        .then((response) => response.json())
+        .then((json) => {
+          setDollar(json);
+          setLoading(false);
+          return json;
+        })
+        .then((dollar) => console.log(dollar));
     };
     fetchData();
   }, []);
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'United States - Monetary base',
+      },
+    },
+  };
+  const labels = [];
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        fill: true,
+        label: 'Monetary base',
+        data: [],
+        borderColor: 'rgb(133, 187, 101)',
+        backgroundColor: 'rgba(133, 187, 101, 0.5)',
+      },
+    ],
+  };
+
+  if (!loading) {
+    labels.push(...dollar.data.dates);
+    data.datasets[0].data.push(...dollar.data.values);
+  }
   return (
     <div>
-      <p data-text="Dollar">Dollar</p>
+      <p data-text="Dollar" style={{ color: '#85BB65', textAlign: 'center' }}>
+        Dollar
+      </p>
+      <h2 style={{ fontSize: '40px', color: 'white', textAlign: 'center' }}>
+        Max Supply :{' '}
+        <span style={{ fontSize: '60px', color: 'white', textAlign: 'center' }}>
+          ∞
+        </span>
+      </h2>
+      <Line options={options} data={data} />
     </div>
   );
 };
